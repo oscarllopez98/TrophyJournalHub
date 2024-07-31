67e738a6c76771d890484403d899911daa33eb53
oscarllopez98 <33796438+oscarllopez98@users.noreply.github.com>
2024-07-31 11:26:47 -0700

    Initial commit for TrophyJournalHub

---
+++ src/test/java/com/example/TrophyJournalHub/TrophyJournalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrophyJournalHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrophyJournalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
